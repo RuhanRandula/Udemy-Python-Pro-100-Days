583fd622cfd8c0faad09a73c3a9282ff4558d160
RuhanRandula <ruhanrn24@gmail.com>
2024-07-05 09:41:28 +0530

    update

---
+++ Day 4 Lesson 13 Heads or tails(aud).py
@@ -0,0 +1,24 @@
+import random
+
+# Generate a random number, either 0 or 1
+coin_toss = random.randint(0, 1)
+
+# Use the random number to determine heads or tails
+if coin_toss == 1:
+    print("Heads")
+else:
+    print("Tails")
+    
+    
+#What DR ANGELA typed
+
+import random
+
+random_side = random.randint(0, 1)
+if random_side == 1:
+  print("Heads")
+else:
+  print("Tails")
+
+
+    
