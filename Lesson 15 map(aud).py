ef45ee11aa2f5b5f410dea1149a017634a9a4ffe
RuhanRandula <ruhanrn24@gmail.com>
2024-08-21 09:21:20 +0530

    map

---
+++ Lesson 15 map(aud).py
@@ -0,0 +1,15 @@
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+# Your code below
+letter = position[0].lower()
+abc = ["a", "b", "c"]
+letter_index = abc.index(letter)
+
+
+
+
+print(f"{line1}\n{line2}\n{line3}")
